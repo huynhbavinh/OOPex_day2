78973c1b51a1211575954491f5906be6f8f855f9
huynhbavinh <55732546+huynhbavinh@users.noreply.github.com>
2019-10-31 15:34:15 +0700

    Add files via upload

---
+++ author.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPex
+{
+    class author
+    {
+        private string name, email;
+        private char gender;
+
+        public author(string name, string email, char sex)
+        {
+            this.name = name;
+            this.email = email;
+            this.gender = sex;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+        public string GetEmail()
+        {
+            return this.email;
+        }
+        public char GetGender()
+        {
+            return this.gender;
+        }
+        public string SetEmail(string newEmail)
+        {
+            return this.email = newEmail;
+        }
+        public override string ToString()
+        {
+            //string sex;
+            //if (this.gender == 1)
+            //{
+            //    sex = "Nam";
+            //}
+            //else
+            //{
+            //    sex = "Nữ";
+            //}
+            return String.Format("Author[name={0},email={1},Gender={2}]",
+                this.name,this.email,gender);
+        }
+    }
+}
---
+++ book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPex
+{
+    class book
+    {
+        private string Name;
+        private author[] authors;
+        private double Price;
+        private int qty;
+
+        public book(string name,author[] authors,double price,int qty =0)
+        {
+            this.Name = name;
+            this.authors = authors;
+            this.Price = price;
+            this.qty = qty;
+        }
+        public string GetName()
+        {
+            return this.Name;
+        }
+        public author[] GetAuthor()
+        {
+            return this.authors;
+        }
+        public double GetPrice()
+        {
+            return this.Price;
+        }
+        public void SetPrice(double price)
+        {
+            this.Price = price;
+        }
+        public int GetQty()
+        {
+            return this.qty;
+        }
+        public void SetQty(int qty)
+        {
+            this.qty = qty;
+        }
+        public override string ToString()
+        {
+            string [] strAuthors = new string[this.authors.Length];
+            for (int i = 0; i < strAuthors.Length; i++)
+            {
+                strAuthors[i] = this.authors[i].ToString();
+            }
+            return String.Format("Book [ name={0},{1},price={2},qty{3}]",this.Name,String.Join(",",strAuthors),this.Price,this.qty);
+        }
+        public string GetAuthorsName()
+        {
+            string[] strAuthors = new string[this.authors.Length];
+            for (int i = 0; i < strAuthors.Length; i++)
+            {
+                strAuthors[i] = this.authors[i].ToString();
+            }
+            return String.Join(",", strAuthors);
+        }
+    }
+}
